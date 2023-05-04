90afc463879799b5cf3e411c18c700ecb93fa85c
Ngonidzashe Brandon Towindo <ngoni@live.com>
2023-05-05 01:24:19 +0200

    Update 7-average_score.sql

--- 0x00-MySQL_Advanced/7-average_score.sql
+++ 0x00-MySQL_Advanced/7-average_score.sql
@@ -1,15 +1,13 @@
 -- script that creates a stored procedure ComputeAverageScoreForUser
 -- that computes and store the average score for a student
-DROP PROCEDURE IF EXISTS ComputeAverageScoreForUserBatch;
+DROP PROCEDURE IF EXISTS ComputeAverageScoreForUser;
 DELIMITER $$
-CREATE PROCEDURE ComputeAverageScoreForUserBatch(
-    IN user_ids VARCHAR(255))
+CREATE PROCEDURE ComputeAverageScoreForUser(
+    IN user_id INT)
 BEGIN
-    DECLARE avg_score FLOAT;
-    SET @query = CONCAT('UPDATE users U, (SELECT user_id, AVG(score) as avg_score FROM corrections WHERE user_id IN (', user_ids, ') GROUP BY user_id) C SET U.average_score = C.avg_score WHERE U.id=C.user_id');
-    PREPARE stmt FROM @query;
-    EXECUTE stmt;
-    DEALLOCATE PREPARE stmt;
+    DECLARE av_score FLOAT;
+    SET av_score = (SELECT AVG(score) FROM corrections AS C WHERE C.user_id=user_id);
+    UPDATE users SET average_score = av_score WHERE id=user_id;
 END
 $$
 DELIMITER ;
