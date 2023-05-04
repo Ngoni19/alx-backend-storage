4a91b9afc91b797434bcd8890e5397d181d1be01
Ngonidzashe Brandon Towindo <ngoni@live.com>
2023-05-05 01:16:09 +0200

    Update 6-bonus.sql

--- 0x00-MySQL_Advanced/6-bonus.sql
+++ 0x00-MySQL_Advanced/6-bonus.sql
@@ -4,15 +4,20 @@ DELIMITER //
 CREATE PROCEDURE AddBonus(
     IN user_id INT,
     IN project_name VARCHAR(255),
-    IN score FLOAT)
+    IN score FLOAT(4,2)
+)
 BEGIN
     DECLARE project_id INT;
-    IF (SELECT COUNT(*) FROM projects WHERE name = project_name) = 0
-    THEN
-        INSERT INTO projects (name) VALUES (project_name);
+    DECLARE project_exists INT;
+
+    SELECT EXISTS (SELECT 1 FROM projects WHERE name = project_name) INTO project_exists;
+    IF project_exists != 1 THEN
+        INSERT IGNORE INTO projects (name) VALUES (project_name);
     END IF;
-    SET project_id = (SELECT id FROM projects WHERE name = project_name LIMIT 1);
-    INSERT INTO corrections (user_id, project_id, score) VALUES(user_id, project_id, score);
+
+    SELECT id INTO project_id FROM projects WHERE name = project_name LIMIT 1;
+    INSERT INTO corrections (user_id, project_id, score) VALUES (user_id, project_id, score);
 END
 //
 DELIMITER ;
+
