8fdd1faed71691f1f72a33f40ccf751e47807b79
Ngonidzashe Brandon Towindo <ngoni@live.com>
2023-05-05 01:07:44 +0200

    Update 6-bonus.sql

--- 0x00-MySQL_Advanced/6-bonus.sql
+++ 0x00-MySQL_Advanced/6-bonus.sql
@@ -1,20 +1,18 @@
--- stored procedure AddBonus that adds a new correction for a student.
-CREATE PROCEDURE AddBonus
-    (
-      @user_id INT,
-      @project_name VARCHAR(50),
-      @score INT
-    )
-AS
+-- Create stored procedure AddBonus that adds a new correction for a student.
+DROP PROCEDURE IF EXISTS AddBonus;
+DELIMITER //
+CREATE PROCEDURE AddBonus(
+    IN user_id INT,
+    IN score FLOAT,
+    IN project_name VARCHAR(255))
 BEGIN
-    -- Check if the project already exists in the table
-    IF NOT EXISTS (SELECT * FROM projects WHERE name = @project_name)
-    BEGIN
-      -- If it doesn't, insert a new project row
-      INSERT INTO projects (name) VALUES (@project_name)
-    END
-
-    INSERT INTO corrections (user_id, project_id, score) 
-    VALUES (@user_id, (SELECT id FROM projects WHERE name = @project_name), @score)
+    DECLARE project_id INT;
+    IF (SELECT COUNT(*) FROM projects WHERE name = project_name) = 0
+    THEN
+        INSERT INTO projects (name) VALUES (project_name);
+    END IF;
+    SET project_id = (SELECT id FROM projects WHERE name = project_name LIMIT 1);
+    INSERT INTO corrections (user_id, project_id, score) VALUES(user_id, project_id, score);
 END
-
+//
+DELIMITER ;
