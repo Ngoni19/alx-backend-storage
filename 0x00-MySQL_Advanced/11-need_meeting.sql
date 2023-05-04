a9f165ba9477c60d92c98020081ed338d3480ea2
Ngonidzashe Brandon Towindo <ngoni@live.com>
2023-05-05 01:39:37 +0200

    Update 11-need_meeting.sql

--- 0x00-MySQL_Advanced/11-need_meeting.sql
+++ 0x00-MySQL_Advanced/11-need_meeting.sql
@@ -2,5 +2,6 @@
 CREATE VIEW need_meeting AS
 SELECT s.name
 FROM students s
-LEFT JOIN meetings m ON s.id = m.student_id AND m.meeting_date >= ADDDATE(CURDATE(), interval -1 MONTH)
+LEFT JOIN meetings m ON s.id = m.student_id AND m.meeting_date >= ADDDATE(CURDATE(),
+interval -1 MONTH)
 WHERE s.score < 80 AND m.id IS NULL;
