d3652e2b1c1ef612a5b773c62ccf77e5a1625a10
Ngonidzashe Brandon Towindo <ngoni@live.com>
2023-05-05 01:48:38 +0200

    Create 100-average_weighted_score.sql

---
+++ 0x00-MySQL_Advanced/100-average_weighted_score.sql
@@ -0,0 +1,17 @@
+-- Script compute weighted average to score
+DROP PROCEDURE IF EXISTS ComputeAverageWeightedScoreForUser;
+DELIMITER //
+CREATE PROCEDURE ComputeAverageWeightedScoreForUser(
+    user_id INT
+)
+BEGIN
+    DECLARE av_w_scrore FLOAT(4,2);
+    SET av_w_scrore = (SELECT SUM(score * weight) / SUM(weight)
+                        FROM users AS U
+                        JOIN corrections as C ON U.id=C.user_id
+                        JOIN projects AS P ON C.project_id=P.id
+                        WHERE U.id=user_id);
+    UPDATE users SET average_score = av_w_scrore WHERE id=user_id;
+END
+//
+DELIMITER ;
