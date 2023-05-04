2c138314c25a9a5264a2fda680b53f13e29ecd46
Ngonidzashe Brandon Towindo <ngoni@live.com>
2023-05-05 01:52:10 +0200

    Update 100-average_weighted_score.sql

--- 0x00-MySQL_Advanced/100-average_weighted_score.sql
+++ 0x00-MySQL_Advanced/100-average_weighted_score.sql
@@ -5,12 +5,12 @@ CREATE PROCEDURE ComputeAverageWeightedScoreForUser(
     user_id INT
 )
 BEGIN
-    DECLARE av_w_scrore FLOAT(4,2);
+    DECLARE av_w_scrore FLOAT();
     SET av_w_scrore = (SELECT SUM(score * weight) / SUM(weight)
                         FROM users AS U
                         JOIN corrections as C ON U.id=C.user_id
                         JOIN projects AS P ON C.project_id=P.id
-                        WHERE id=user_id);
+                        WHERE U.id=user_id);
     UPDATE users SET average_score = av_w_scrore WHERE id=user_id;
 END
 //
