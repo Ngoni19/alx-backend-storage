9695e0c21ee43ca6d7af3edb7e963ad63584ab83
Ngonidzashe Brandon Towindo <ngoni@live.com>
2023-05-05 00:42:47 +0200

    Update 5-valid_email.sql

--- 0x00-MySQL_Advanced/5-valid_email.sql
+++ 0x00-MySQL_Advanced/5-valid_email.sql
@@ -3,9 +3,9 @@ CREATE TRIGGER change_email
 BEFORE UPDATE ON users
 FOR EACH ROW
 BEGIN
-    DECLARE result_code INT;
-    CALL sp_TransferData(result_code);
-    IF result_code < 0 THEN
+    DECLARE valid_email INT;
+    CALL sp_TransferData(valid_email);
+    IF valid_email < 0 THEN
         SET NEW.email = OLD.email;
     END IF;
 END;
