c2c72d632a8d85f4b5e949c1dd16adcfb06a4c10
Ngonidzashe Brandon Towindo <ngoni@live.com>
2023-05-31 00:23:14 +0200

    Create 10-update_topics.py

---
+++ 0x01-NoSQL/10-update_topics.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+""" Script -> Update School """
+import pymongo
+from typing import List
+
+
+def update_topics(mongo_collection, name, topics):
+    """ Method: Change the data
+    
+        Args:
+            mongo_collection:
+            name: School
+            topics: School name
+
+        Return:
+            None
+    """
+    query: dict = {'name': name}
+    mongo_collection.update_many(query, {"$set": {"topics": topics}})
+
